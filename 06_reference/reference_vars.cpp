8052065b197d6edd9a9683ee129c6f5664195810
BryanEdou456 <bedou9121@floridapoly.edu>
2026-02-27 14:23:03 +0000

    HELLLLPPPP

---
+++ 06_reference/reference_vars.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void) {
+    int a = 10;
+    // Create a reference variable
+    int& a_ref = a;
+
+    a += 10;
+    std::cout << a << ' ' << a_ref << std::endl;
+
+    add_five(a);
+    std::cout << a << ' '<< a_ref << std::endl;
+    return 0;
+}
+
+void add_five(int& num) { 
+    num += 5;
+}
