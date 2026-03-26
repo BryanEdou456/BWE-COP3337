7d34d705b4feb5375bf96f3ae9b6fc76752cc5dc
BryanEdou456 <bedou9121@floridapoly.edu>
2026-03-26 03:24:39 +0000

    Incomplete code from previous lectures.

---
+++ MyVectorFiles/MyVector.cpp
@@ -0,0 +1,31 @@
+#include "MyVector.hpp"
+#include <iostream>
+MyVector::MyVector(int capacity) {
+this->capacity = capacity;
+size = 0;
+elements = new int[capacity];
+}
+// Copy constructor
+MyVector::MyVector(const MyVector& other) {
+capacity = other.capacity;
+size = other.size;
+elements = new int[capacity];
+for(int i = 0; i < size; i++) {
+elements[i] = other.elements[i];
+}
+}
+MyVector::~MyVector() {
+delete [] elements;
+}
+void MyVector::print() const {
+std::cout << "[ ";
+for (int i = 0; i < size; i++) {
+std::cout << elements[i] << ' ';
+}
+std::cout << "]\n";
+}
+void MyVector::push_back(int val) {
+// TODO consider the case when the vector is full
+elements[size] = val;
+size++;
+}
---
+++ MyVectorFiles/MyVector.hpp
@@ -0,0 +1,16 @@
+#ifndef MYVECTOR_HPP
+#define MYVECTOR_HPP
+class MyVector {
+public:
+MyVector(int capacity = 10);
+MyVector(const MyVector& other);
+// Destructor
+~MyVector();
+void print() const;
+void push_back(int val); // Add the new element at the end
+private:
+int* elements; // An array of integers
+int size; // The current number of elements
+int capacity; // The max number of elements in the array
+};
+#endif
